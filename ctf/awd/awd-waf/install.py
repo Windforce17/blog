9f32469feb480a45cd391f8843e4d54f60ebe7b7
Travis Builder <wzcboss@qq.com>
2019-05-05 03:18:37 +0000

    Deployed 9927f44 with MkDocs version: 1.0.4

--- ctf/awd/awd-waf/install.py
+++ ctf/awd/awd-waf/install.py
@@ -1,30 +1,35 @@
 import os
 # find /var/www/html ‐type f ‐path "*.php" | xargs sed ‐i "s/<?php/<?
 # php\nrequire_once('\/tmp\/waf.php');\n/g"
-base_dir = '/Users/apple/Documents/data' #web路径
+#
+# vim php.ini
+#auto_append_file = "/dir/path/phpwaf.php"
+base_dir = '/Users/apple/Documents/data'  # web路径
 
-def scandir(startdir) :
-    
+
+def scandir(startdir):
+
     os.chdir(startdir)
-    for obj in os.listdir(os.curdir) :
+    for obj in os.listdir(os.curdir):
         path = os.getcwd() + os.sep + obj
         if os.path.isfile(path) and '.php' in obj:
-        	modifyip(path,'<?php','<?php\nrequire_once(\'waf.php\');') #强行加一句代码
-        if os.path.isdir(obj) :
+            # 强行加一句代码
+            modifyip(path, '<?php', '<?php\nrequire_once(\'waf.php\');')
+        if os.path.isdir(obj):
             scandir(obj)
-            os.chdir(os.pardir) 
+            os.chdir(os.pardir)
 
-def modifyip(tfile,sstr,rstr):
+
+def modifyip(tfile, sstr, rstr):
     try:
-        lines=open(tfile,'r').readlines()
-        flen=len(lines)-1
+        lines = open(tfile, 'r').readlines()
+        flen = len(lines)-1
         for i in range(flen):
             if sstr in lines[i]:
-                lines[i]=lines[i].replace(sstr,rstr)
-        open(tfile,'w').writelines(lines)
-        
-    except Exception,e:
+                lines[i] = lines[i].replace(sstr, rstr)
+        open(tfile, 'w').writelines(lines)
+    except Exception, e:
         print e
-        
+
 
 scandir(base_dir)
